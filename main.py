31f5e0bf3c0fe7df037370e060afdabe55ffaca9
kleckus <48417779+kleckus@users.noreply.github.com>
2019-03-11 22:07:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from game import Game
 
 def main():
-	jogo = Game(2 , 1000)
+	jogo = Game(3 , 1000)
+	#jogo.showCardsOnHand()
 	jogo.givePlayersCards()
 	jogo.showCardsOnHand()
 	jogo.Flop()
-	jogo.showCardsOnTable()
-	jogo.turn()
-	jogo.showCardsOnTable()
+	jogo.showFlop()
+	jogo.Turn()
+	jogo.showTurn()
 	jogo.River()
+	jogo.showRiver()
 	jogo.showCardsOnTable()
 
 main()
