e0cc8196e9be35c8b8e0d1fddc4db3b190be264a
kleckus <48417779+kleckus@users.noreply.github.com>
2019-03-11 22:08:39 -0300

    Update player.py

--- player.py
+++ player.py
@@ -1,13 +1,14 @@
-class Player:
+from cartas import Cards
+class Player(Cards):
 	def __init__(self, monay = 500):
 		self.__hand = list()
 		self.__monay = monay
 	
 	def showHand(self):
-		print("Your hand: " + self.__hand[0].showCard() + " and " + self.__hand[1].showCard())
+		print("Sua mão: " + self.__hand[0].showCard() + " e " + self.__hand[1].showCard())
 	
 	def getMonay(self):
 		return self.__monay
 	
-	def addToHand(self, Card):
-		self.__append(card)
+	def addToHand(self, card):
+		self.__hand.append(card)
