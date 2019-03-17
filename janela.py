7ce204634c6a6b05624c202243e8defff71c4eba
YohanAlexander <yohanfranca@gmail.com>
2019-03-16 22:55:17 -0300

    background

--- janela.py
+++ janela.py
@@ -3,18 +3,18 @@ from functools import partial
 
 janela = Tk()
 janela.title("Poker Game")
-janela["background"] = "white"
-janela.geometry("400x400") # Largura x Altura + Esquerda + Topo
 
-ed = Entry(janela)
-ed.place(x = 100, y = 100)
+table = PhotoImage(file='Baralho\mesa.png')
+w = table.width()
+h = table.height()
 
-def click(botao):
-    print(botao["text"])
-    print(ed.get())
+janela.geometry("%dx%d+0+0" %(w, h))
 
-bt = Button(janela, width = 16, text = "Jogar")
-bt.place(x = 100, y = 200)
-bt["command"] = partial(click, bt)
+fundo = Label(janela, image=table)
+fundo.place(x=0, y=0, relwidth=1, relheight=1)
+
+#card = PhotoImage(file='Baralho\carta.png')
+#carta = Label(janela, image=card)
+#carta.pack(side=BOTTOM)
 
 janela.mainloop()
